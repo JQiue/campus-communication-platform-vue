b04686bb1f6f968571b9dbeb666887624a317e32
datareachable-wangjinqiu <jinqiu.wang@datareachable.com>
2022-04-22 09:32:26 +0800

    ada

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import axios from "axios";
 
-Vue.config.productionTip = false
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.withCredentials = true;
+
+Vue.prototype.$http = axios;
+Vue.prototype.$message = ElementUI.Message;
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Home from "../views/Home.vue";
+import Edit from "../views/Edit.vue";
+import Userinfo from "../views/Userinfo.vue";
+import Test from "../components/Test.vue";
+import Transaction from "../components/Transaction.vue";
+import Article from "../components/Article.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    redirect: "/login",
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+    children: [
+      { path: "/home", redirect: "article" },
+      { path: "test", component: Test },
+      { path: "transaction", component: Transaction },
+      { path: "article", component: Article },
+      { path: "edit", component: Edit },
+      { path: "userinfo", component: Userinfo },
+    ],
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
